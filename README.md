6ebe7abd8ee147d22cd1e3eaccc5f4be9925290d
Ailson <ailsonsixseven@gmail.com>
2024-02-03 11:35:38 -0300

    feat(convtemp): first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+<h1 align='center'> Conversor de Temperatura </h1>
+
+<p align='center'>Este é um programa feito em C converte temperaturas entre `Celsius`, `Fahrenheit` e `Kelvin`.</p>
+
+<div align='center'>
+  <a href="https://pt.wikipedia.org/wiki/C_(linguagem_de_programa%C3%A7%C3%A3o)" target="_blank">
+      <img src="./img/c_logo.png" width="150" height="150" />
+  </a>
+</div>
+
+## 🔧 Ferramentas
+
+-   C: é uma linguagem de programação de alto nível. Sendo considerada a mãe das linguagens de programação modernas.
+-   GCC: é um conjunto de compiladores para a linguagem C, produzidos pelo projeto GNU.
+
+
+## 💻 Como Executar o Projeto
+
+- O projeto pode ser executado normalmente em um ambiente Linux.
+- Também pode ser recompilado para que funcione em ambientes Windows.
+
+1. Compilando o projeto  
+  ```shell
+    gcc - o nomedoprojeto nomedoprojeto.c
+  ```
+
+  ou
+
+  ```shell
+    make nomedoprojeto
+  ```
+2. Executando projteo (Linux):
+
+- Caminhe até o local onde o executável do projeto se encontra.
+- Execute com o comando abaixo.
+
+  ```shell
+    ./nomedoprojeto
+  ```
+## ⚙️ Forma de Uso:
+
+- O usuário deve executar o programa e entrar com uma sequência de valor e argumento:
+    - Exemplo: 0 -h
+
+###### Aqui está uma lista com os possíveis argumentos:
+      
+      Unidade:
+      '-c' ou '--celsius':    Converte de Celsius para Fahrenheit e Kelvin
+      '-f' ou '--fahrenheit': Converte de Fahrenheit para Celsius e Kelvin
+      '-k' ou '--kelvin':     Converte de Kelvin para Celsius e Fahrenheit
+      '-h' ou '--help':       Mostra a tela de ajuda
+
+## 🙏 Contribuições:
+
+Contribuições são bem-vindas! Sinta-se à vontade para enviar problemas, solicitações de pull e feedback.
+
---
+++ conversordetemperatura.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <string.h>
+
+float val;
+int argC;
+char arg[50];
+
+void CelsiusConv(float val){
+  float result_fahrenheit;
+  float result_kelvin;
+
+  result_fahrenheit = (val * 9/5) + 32;
+  result_kelvin = val + 273.15;
+
+  printf("Temperatura em fahrenheit: %.2f ºF\n", result_fahrenheit);
+  printf("Temperatura em Kelvin: %.2f K\n", result_kelvin);
+
+}
+
+void FahrenheitConv(float val){
+  float result_celsius;
+  float result_kelvin;
+
+  result_celsius = (val - 32) * 5/9;
+  result_kelvin = (val + 459.67) * 5/9;
+
+  printf("Temperatura em celsius: %.2f C\n", result_celsius);
+  printf("Temperatura em kelvin: %.2f K\n", result_kelvin);
+}
+
+void KelvinConv(float val){
+  float result_celsius;
+  float result_fahrenheit;
+
+  result_celsius = val - 273.15;
+  result_fahrenheit = val * 9/5 - 459.67;
+
+  printf("Temperatura em celsius: %.2f C\n", result_celsius);
+  printf("Temperatura em fahrenheit: %.2f ºF\n", result_fahrenheit);
+}
+
+void help(){
+  printf("Conversor de Temperatura\n");
+  printf("---------------------------\n");
+  printf("Uso: [temperatura] [argumento]\n\n");
+
+  printf("Argumentos:\n");
+  printf("  -c || --celsius\t\tConverte de Celsius para Fahrenheit e Kelvin\n");
+  printf("  -f || --fahrenheit\t\tConverte de Fahrenheit para Celsius e Kelvin\n");
+  printf("  -k || --kelvin\t\tConverte de Kelvin para Celsius e Fahrenheit\n");
+  printf("  -h || --help\t\t\tMostra a tela de ajuda\n\n");
+}
+
+int main(int argc, char *argv[]){
+  
+  printf("  Conversor de Temperatura v1.0\n\n");
+  printf("  Autor: Ailson Guedes\n\n");
+  printf("  Você pode utilizar o valor 0 e o argumento -h ou --help para acessar o menu de ajuda.\n");
+
+  printf(">>>>convshell: ");
+  scanf("%f %s", &val, &arg);
+  printf("\n");
+  
+  if (strcmp(arg,"-c") == 0 || strcmp(arg, "--celsius") == 0){
+    argC = 1;
+  } else if (strcmp(arg, "-f") == 0 || strcmp(arg, "--fahrenheit") ==0 ){
+    argC = 2;
+  } else if (strcmp(arg, "-k") == 0 || strcmp(arg, "--kelvin") ==0 ){
+    argC = 3;
+  } else if (strcmp(arg, "-h") == 0 || strcmp(arg, "--help") ==0 ){
+    argC = 4;
+  } else {
+    argC = 0;
+  }
+
+  switch ( argC ){
+
+    case 1:
+      CelsiusConv(val);
+    break;
+    
+    case 2:
+      FahrenheitConv(val);
+    break;
+
+    case 3:
+      KelvinConv(val);
+    break;
+
+    case 4:
+      help();
+    break;
+
+    default:
+      printf("Argumento inválido!\n");
+  }
+
+  return 0;
+}
